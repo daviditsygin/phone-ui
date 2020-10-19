520e72ea8d010a1e007e346059038cd3932de65e
daviditsygin <daviditsygin@gmail.com>
2020-10-18 19:56:59 -0700

    styling fixes

--- src/App.js
+++ src/App.js
@@ -122,66 +122,56 @@ function App() {
         background: colors.gray800,
       }}
     >
-      <div className="flex h-full  ">
-        <div
-          className="w-1/4 hidden text-white p-8"
-          style={{
-            background: colors.gray600,
-          }}
-        >
-          worknumber
-        </div>
-        {user ? <></> : <></>}
-        <div className="w-full text-white ">
-          <div
-            className="w-full p-4 text-left"
-            style={{
-              background: colors.gray700,
-            }}
-          >
-            <p>worknumber</p>
-          </div>
-          <div className="lg:p-8 p-4 container mx-auto" id="router">
-            <h2 className="text-left mb-2">Choose your work number.</h2>
-            <Hello
-              updateText={(t) => {
-                console.log("got this from updateText", t);
+      <StyletronProvider value={engine}>
+        <BaseProvider theme={DarkTheme}>
+          <div className="flex h-full  ">
+            <div
+              className="w-1/4 hidden text-white p-8"
+              style={{
+                background: colors.gray600,
+              }}
+            >
+              worknumber
+            </div>
+            {user ? <></> : <></>}
+            <div className="w-full text-white ">
+              <div
+                className="w-full p-4 text-left"
+                style={{
+                  background: colors.gray700,
+                }}
+              >
+                <p>worknumber</p>
+              </div>
+              <div className="lg:p-8 p-4 container mx-auto" id="router">
+                <h2 className="text-left mb-2">Choose your work number.</h2>
+                <Hello
+                  updateText={(t) => {
+                    console.log("got this from updateText", t);
 
-                getNumbers(t.substr(0, 3), t.substr(3));
-                setIdx(0);
-              }}
-              keyUp={(e) => {
-                if (e.which === 40) {
-                  setIdx(idx > numbers.length - 1 ? 0 : idx + 1);
-                }
-                if (e.which === 38) {
-                  setIdx(idx > 0 ? idx - 1 : numbers.length - 1);
-                }
-                if (e.which === 13) {
-                  setConfirm(numbers[idx]);
-                }
-              }}
-            />
-            {loading ? (
-              <div className="w-16 mx-auto pt-4">
-                <Spinner
-                  overrides={{
-                    ActivePath: {
-                      style: ({ $theme }) => {
-                        return {
-                          outline: `${$theme.colors.warning200} solid`,
-                          backgroundColor: $theme.colors.accent50,
-                        };
-                      },
-                    },
+                    getNumbers(t.substr(0, 3), t.substr(3));
+                    setIdx(0);
+                  }}
+                  keyUp={(e) => {
+                    if (e.which === 40) {
+                      setIdx(idx > numbers.length - 1 ? 0 : idx + 1);
+                    }
+                    if (e.which === 38) {
+                      setIdx(idx > 0 ? idx - 1 : numbers.length - 1);
+                    }
+                    if (e.which === 13) {
+                      setConfirm(numbers[idx]);
+                    }
                   }}
                 />
-              </div>
-            ) : (
-              <></>
-            )}
-            <StyletronProvider value={engine}>
-              <BaseProvider theme={DarkTheme}>
+                {loading ? (
+                  <div className="w-16 mx-auto pt-4">
+                    <Spinner />
+                  </div>
+                ) : (
+                  <></>
+                )}
+
                 <Modal
                   onClose={() => setConfirm(null)}
                   closeable={!binding}
@@ -216,33 +206,37 @@ function App() {
                     </ModalButton>
                   </ModalFooter>
                 </Modal>
-              </BaseProvider>
-            </StyletronProvider>
-            <ul className="text-left">
-              {numbers.map((n, i) => {
-                return (
-                  <li
-                    className="p-2 pl-16 hover:text-purple-600 transition duration-300"
-                    style={{
-                      background: i == idx ? colors.gray700 : colors.gray900,
-                      color: i == idx ? colors.purple200 : colors.gray200,
-                    }}
-                    onMouseOver={(e) => setIdx(i)}
-                    onClick={(e) => {
-                      setConfirm(numbers[i]);
-                    }}
-                  >
-                    <p className="text-sm">{n.friendlyName}</p>
-                    <p className="text-xs">
-                      {n.locality ? n.locality : n.region + ", " + n.isoCountry}
-                    </p>
-                  </li>
-                );
-              })}
-            </ul>
+
+                <ul className="text-left">
+                  {numbers.map((n, i) => {
+                    return (
+                      <li
+                        className="p-2 pl-16 hover:text-purple-600 transition duration-300"
+                        style={{
+                          background:
+                            i == idx ? colors.gray700 : colors.gray900,
+                          color: i == idx ? colors.purple200 : colors.gray200,
+                        }}
+                        onMouseOver={(e) => setIdx(i)}
+                        onClick={(e) => {
+                          setConfirm(numbers[i]);
+                        }}
+                      >
+                        <p className="text-sm">{n.friendlyName}</p>
+                        <p className="text-xs">
+                          {n.locality
+                            ? n.locality
+                            : n.region + ", " + n.isoCountry}
+                        </p>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        </BaseProvider>
+      </StyletronProvider>
     </div>
   );
 }
