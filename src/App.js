69e806d241512fd4c8c45efbd9cd28fa547669c1
daviditsygin <daviditsygin@gmail.com>
2020-10-18 19:54:18 -0700

    loading progress, styling changes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,26 @@ import { Client as Styletron } from "styletron-engine-atomic";
 import { Provider as StyletronProvider } from "styletron-react";
 import { colors } from "baseui/tokens";
 import { LightTheme, DarkTheme, BaseProvider, styled } from "baseui";
-import { PhoneInput, COUNTRIES, SIZE } from "baseui/phone-input";
-import { StatefulInput } from "baseui/input";
+import { PhoneInput, COUNTRIES } from "baseui/phone-input";
+import { Spinner } from "baseui/spinner";
+import {
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  ModalButton,
+  SIZE,
+  ROLE,
+} from "baseui/modal";
+import { KIND as ButtonKind } from "baseui/button";
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+  // dev code
+  localStorage.setItem("url", process.env.REACT_APP_FUNCTION_DEV);
+} else {
+  // production code
+  localStorage.setItem("url", process.env.REACT_APP_FUNCTION_URL);
+}
+
 const engine = new Styletron();
 const Centered = styled("div", {
   display: "flex",
@@ -27,7 +45,6 @@ function Hello({ updateText, keyUp }) {
           <PhoneInput
             clearable
             country={COUNTRIES.US}
-            size={SIZE.compact}
             text={text}
             type={"number"}
             onTextChange={(e) => setText(e.currentTarget.value)}
@@ -37,6 +54,17 @@ function Hello({ updateText, keyUp }) {
               }
             }}
             onKeyUp={(e) => keyUp(e)}
+            overrides={{
+              CountrySelect: {
+                style: ({ $theme }) => {
+                  return {
+                    outline: `${$theme.colors.warning600} solid`,
+                    backgroundColor: $theme.colors.warning600,
+                    display: "none",
+                  };
+                },
+              },
+            }}
           />
         </Centered>
       </BaseProvider>
@@ -48,10 +76,15 @@ function App() {
   const [user, setUser] = React.useState(null);
   const [numbers, setNumbers] = React.useState([]);
   const [idx, setIdx] = React.useState(0);
+  const [loading, setLoading] = React.useState(true);
+  const [confirm, setConfirm] = React.useState(null);
+  const [binding, setBinding] = React.useState(false);
 
   const getNumbers = async (areaCode, contains) => {
+    setLoading(true);
+    setNumbers([]);
     let res = await fetch(
-      process.env.REACT_APP_FUNCTION_URL +
+      localStorage.getItem("url") +
         "/lookUpNumbers" +
         (areaCode ? "?areaCode=" + areaCode : "") +
         (contains ? "&contains=" + contains : ""),
@@ -63,7 +96,17 @@ function App() {
     );
     let json = await res.json();
     setNumbers(json);
+    setLoading(false);
     console.log(numbers);
+  };
+
+  const confirmNumber = async () => {
+    console.log("confirming", confirm.friendlyName);
+    setBinding(true);
+    setTimeout(() => {
+      setConfirm(null);
+      setBinding(false);
+    }, 5000);
   };
 
   useEffect(() => {
@@ -73,59 +116,131 @@ function App() {
   }, []);
 
   return (
-    <div className="App bg-gray-800">
-      <div className="flex h-full ">
+    <div
+      className="App"
+      style={{
+        background: colors.gray800,
+      }}
+    >
+      <div className="flex h-full  ">
         <div
-          className="w-1/4 text-white p-8"
+          className="w-1/4 hidden text-white p-8"
           style={{
             background: colors.gray600,
           }}
         >
-          Sidebar
+          worknumber
         </div>
         {user ? <></> : <></>}
-        <div
-          className="w-full bg-gray-800 text-white p-8"
-          style={{
-            background: colors.gray800,
-          }}
-        >
-          <h2 className="text-left mb-2">Choose your work phone number.</h2>
-          <Hello
-            updateText={(t) => {
-              console.log("got this from updateText", t);
+        <div className="w-full text-white ">
+          <div
+            className="w-full p-4 text-left"
+            style={{
+              background: colors.gray700,
+            }}
+          >
+            <p>worknumber</p>
+          </div>
+          <div className="lg:p-8 p-4 container mx-auto" id="router">
+            <h2 className="text-left mb-2">Choose your work number.</h2>
+            <Hello
+              updateText={(t) => {
+                console.log("got this from updateText", t);
 
-              getNumbers(t.substr(0, 3), t.substr(3));
-              setIdx(0);
-            }}
-            keyUp={(e) => {
-              if (e.which === 40) {
-                setIdx(idx > numbers.length - 1 ? 0 : idx + 1);
-              }
-              if (e.which === 38) {
-                setIdx(idx > 0 ? idx - 1 : numbers.length - 1);
-              }
-            }}
-          />
-          <ul className="text-left">
-            {numbers.map((n, i) => {
-              return (
-                <li
-                  className="p-2 pl-16 hover:text-purple-600 transition duration-300"
-                  style={{
-                    background: i == idx ? colors.gray700 : colors.gray900,
-                    color: i == idx ? colors.purple200 : colors.gray200,
+                getNumbers(t.substr(0, 3), t.substr(3));
+                setIdx(0);
+              }}
+              keyUp={(e) => {
+                if (e.which === 40) {
+                  setIdx(idx > numbers.length - 1 ? 0 : idx + 1);
+                }
+                if (e.which === 38) {
+                  setIdx(idx > 0 ? idx - 1 : numbers.length - 1);
+                }
+                if (e.which === 13) {
+                  setConfirm(numbers[idx]);
+                }
+              }}
+            />
+            {loading ? (
+              <div className="w-16 mx-auto pt-4">
+                <Spinner
+                  overrides={{
+                    ActivePath: {
+                      style: ({ $theme }) => {
+                        return {
+                          outline: `${$theme.colors.warning200} solid`,
+                          backgroundColor: $theme.colors.accent50,
+                        };
+                      },
+                    },
                   }}
-                  onMouseOver={(e) => setIdx(i)}
+                />
+              </div>
+            ) : (
+              <></>
+            )}
+            <StyletronProvider value={engine}>
+              <BaseProvider theme={DarkTheme}>
+                <Modal
+                  onClose={() => setConfirm(null)}
+                  closeable={!binding}
+                  isOpen={confirm}
+                  autoFocus
+                  size={SIZE.default}
+                  role={ROLE.dialog}
+                  style={{ textAlign: "left" }}
                 >
-                  <p className="text-sm">{n.friendlyName}</p>
-                  <p className="text-xs">
-                    {n.locality ? n.locality : n.region + ", " + n.isoCountry}
-                  </p>
-                </li>
-              );
-            })}
-          </ul>
+                  <ModalHeader>
+                    {confirm ? confirm.friendlyName : "..."}
+                  </ModalHeader>
+                  <ModalBody>
+                    Use this as your work number?
+                    <p style={{ opacity: 0.5, fontSize: 12, marginTop: 10 }}>
+                      You can't change your number once you've confirmed it.
+                    </p>
+                  </ModalBody>
+                  <ModalFooter>
+                    {!binding ? (
+                      <ModalButton
+                        kind={ButtonKind.tertiary}
+                        onClick={() => setConfirm(null)}
+                      >
+                        Cancel
+                      </ModalButton>
+                    ) : (
+                      <></>
+                    )}
+                    <ModalButton onClick={confirmNumber} isLoading={binding}>
+                      Confirm
+                    </ModalButton>
+                  </ModalFooter>
+                </Modal>
+              </BaseProvider>
+            </StyletronProvider>
+            <ul className="text-left">
+              {numbers.map((n, i) => {
+                return (
+                  <li
+                    className="p-2 pl-16 hover:text-purple-600 transition duration-300"
+                    style={{
+                      background: i == idx ? colors.gray700 : colors.gray900,
+                      color: i == idx ? colors.purple200 : colors.gray200,
+                    }}
+                    onMouseOver={(e) => setIdx(i)}
+                    onClick={(e) => {
+                      setConfirm(numbers[i]);
+                    }}
+                  >
+                    <p className="text-sm">{n.friendlyName}</p>
+                    <p className="text-xs">
+                      {n.locality ? n.locality : n.region + ", " + n.isoCountry}
+                    </p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
